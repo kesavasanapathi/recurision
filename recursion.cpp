0c8843783b67c9a8a4459b9f17b377cefe8b6095
kesavasanapathi <84114804+kesavasanapathi@users.noreply.github.com>
2021-10-06 20:59:53 +0400

    Add files via upload

---
+++ recursion.cpp
@@ -0,0 +1,27 @@
+
+public class Main
+{
+
+public static void main(String[] args) {
+
+fun(5);
+
+}
+
+static void fun(int n){
+
+if (n==0){
+	 
+return;
+	   
+}
+	
+System.out.println(n);
+	
+fun(n-1);
+	
+System.out.println(n);
+
+}
+
+}
