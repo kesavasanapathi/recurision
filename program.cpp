94b17dfe4798ae5a28b66cf877e3e293ea4ff0c1
kesavasanapathi <84114804+kesavasanapathi@users.noreply.github.com>
2021-10-07 16:27:46 +0400

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,9 @@
+https://ide.geeksforgeeks.org/iOFg0uwJAQ         -> print the number from n to 1
+https://ide.geeksforgeeks.org/YCib8ne6BI         -> print the number from 1 t0 n
+https://ide.geeksforgeeks.org/iged83wcOv         -> factorial of a  any number
+https://ide.geeksforgeeks.org/BQZFsB4hwk         ->sum of  n number
+https://ide.geeksforgeeks.org/fINCLENg8x         ->sum of the digit of a given number
+https://ide.geeksforgeeks.org/0eWG1IK0pv         -> product of the digit of a given number
+https://ide.geeksforgeeks.org/52rPzp5Wqe         ->reverse of a given number
+https://ide.geeksforgeeks.org/n2VONFY1cu         ->palindrome of number
+https://ide.geeksforgeeks.org/Zyuwo7RfN7         ->count number of zero in a number
